52681f6ec70177cbe2a3c48f374c141208c62ebb
Shreya Katuwal <shreyakatuwal@outlook.com>
2018-11-30 12:28:29 +0545

    after part 9

--- app/src/main/java/com/apps/shreya/chatapplication/ProfileFragment.java
+++ app/src/main/java/com/apps/shreya/chatapplication/ProfileFragment.java
@@ -1,12 +1,25 @@
 package com.apps.shreya.chatapplication;
 
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by Shreya on 16/11/2018.
@@ -14,10 +27,37 @@ import android.view.ViewGroup;
 
 public class ProfileFragment extends Fragment {
 
+    CircleImageView image_profile;
+    TextView username1;
+
+
+    DatabaseReference reference;
+    FirebaseUser fuser;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
 
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        image_profile = view.findViewById(R.id.profile_pic);
+        username1 = view.findViewById(R.id.username);
+
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+        reference= FirebaseDatabase.getInstance().getReference("Users").child(fuser.getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //User user = dataSnapshot.getValue(User.class);
+                //username1.setText(user.getUsername);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return view;
     }
 
     //TO HIDE TOOLBAR FROM FRAGMENT
--- app/src/main/java/com/apps/shreya/chatapplication/SignUpPage.java
+++ app/src/main/java/com/apps/shreya/chatapplication/SignUpPage.java
@@ -1,10 +1,12 @@
 package com.apps.shreya.chatapplication;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,6 +20,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.HashMap;
 
 //helps you signup the new user
 // consists of name of user,email,password
@@ -27,6 +34,11 @@ public class SignUpPage extends AppCompatActivity {
     private TextInputLayout mEmail;
     private TextInputLayout mPassword;
     private Button mCreateButton;
+
+    private DatabaseReference mDatabase;
+
+    //ProgressDialog
+    private ProgressDialog mRegProgress;
 
     //firebase auth
     private FirebaseAuth mAuth;
@@ -40,6 +52,8 @@ public class SignUpPage extends AppCompatActivity {
 
         //FirebaseAuth
         mAuth = FirebaseAuth.getInstance();
+
+        mRegProgress = new ProgressDialog(this);
 
         tv_login = (TextView)findViewById(R.id.link_login);
 
@@ -57,7 +71,17 @@ public class SignUpPage extends AppCompatActivity {
                 String email = mEmail.getEditText().getText().toString();
                 String password = mPassword.getEditText().getText().toString();
 
-                register_user(name, email, password);   //another method to reg, passing values
+                if(!TextUtils.isEmpty(name) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)){
+
+                    mRegProgress.setTitle("Registering User");
+                    mRegProgress.setMessage("Please wait while we create your account !");
+                    mRegProgress.setCanceledOnTouchOutside(false);
+                    mRegProgress.show();
+
+                    register_user(name, email, password);
+
+                }
+
 
             }
         });
@@ -72,7 +96,7 @@ public class SignUpPage extends AppCompatActivity {
         });
     }
 
-    public void register_user(String name, String email, String password){
+    public void register_user(final String name, String email, String password){
 
         //firebase auth --> signup new user
         mAuth.createUserWithEmailAndPassword(email, password)
@@ -81,10 +105,42 @@ public class SignUpPage extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
 
-                            Intent mainIntent = new Intent(SignUpPage.this, MainActivity.class);
+                           /* Intent mainIntent = new Intent(SignUpPage.this, MainActivity.class);
+                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK); //TO NOT GO BACK TO START ACTIVITY WHEN LOGGED IN
                             startActivity(mainIntent);
-                            finish();
+                            finish();*/
 
+                            FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+                            String uid = current_user.getUid();
+
+                            mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+                            String device_token = FirebaseInstanceId.getInstance().getToken();
+
+                            HashMap<String, String> userMap = new HashMap<>();
+                            userMap.put("name", name);
+                            userMap.put("status", "Hi there I'm using Lapit Chat App.");
+                            userMap.put("image", "default");
+                            userMap.put("thumb_image", "default");
+                            userMap.put("device_token", device_token);
+
+                            mDatabase.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    if(task.isSuccessful()){
+
+                                        mRegProgress.dismiss();
+
+                                        Intent mainIntent = new Intent(SignUpPage.this, MainActivity.class);
+                                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(mainIntent);
+                                        finish();
+
+                                    }
+
+                                }
+                            });
                         } else {
                             // If sign in fails, display a message to the user.
                             Toast.makeText(SignUpPage.this, "Password length: 6+",
