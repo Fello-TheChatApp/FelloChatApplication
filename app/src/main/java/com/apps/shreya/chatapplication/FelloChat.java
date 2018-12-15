e698943a850e3adddf800529088b5fc13ec20adc
Shreya Katuwal <shreyakatuwal@outlook.com>
2018-12-15 15:58:26 +0545

    chat works

--- app/src/main/java/com/apps/shreya/chatapplication/FelloChat.java
+++ app/src/main/java/com/apps/shreya/chatapplication/FelloChat.java
@@ -2,29 +2,66 @@ package com.apps.shreya.chatapplication;
 
 import android.app.Application;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
 
 
 
-public class FelloChat extends Application {
+public class FelloChat extends Application  {
 
-public void onCreate() {
+    private DatabaseReference mUserDatabase;
+    private FirebaseAuth mAuth;
 
-    super.onCreate();
+    public void onCreate() {
 
-    FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        super.onCreate();
 
-    Picasso.Builder builder=new Picasso.Builder(this);
-    builder.downloader(new OkHttpDownloader(this, Integer.MAX_VALUE));
-    Picasso built = builder.build();
-    built.setIndicatorsEnabled(true);
-    built.setLoggingEnabled(true);
-    Picasso.setSingletonInstance(built);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+          /* Picasso */
+
+        Picasso.Builder builder = new Picasso.Builder(this);
+        builder.downloader(new OkHttpDownloader(this, Integer.MAX_VALUE));
+        Picasso built = builder.build();
+        built.setIndicatorsEnabled(true);
+        built.setLoggingEnabled(true);
+        Picasso.setSingletonInstance(built);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() != null) {
+
+            mUserDatabase = FirebaseDatabase.getInstance()
+                    .getReference().child("Users").child(mAuth.getCurrentUser().getUid());
+
+            mUserDatabase.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    if (dataSnapshot != null) {
+
+                        mUserDatabase.child("online").onDisconnect().setValue(ServerValue.TIMESTAMP);
+
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+        }
 
 
-
-}
+    }
 
 }
